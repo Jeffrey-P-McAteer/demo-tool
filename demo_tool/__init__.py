f44192a554e7dcffd1cc203e3261d96e7861e701
McAteer, Jeffrey P CIV STRATCOM JWAC (US) <jeffrey.p.mcateer.civ@mail.mil>
2023-01-13 12:52:22 -0500

    'signal

--- demo_tool/__init__.py
+++ demo_tool/__init__.py
@@ -2,8 +2,22 @@
 import os
 import sys
 import subprocess
+import time
+import signal
+
+def signal_handler(signal, frame):
+    print('In signal_handler')
+    sys.exit(0)
 
 def main(args=sys.argv):
+    signal.signal(signal.SIGINT, signal_handler)
     print(f'args={args}')
+    while True:
+        time.sleep(0.5)
+        print('Doing work...')
+
+
+
+
 
 

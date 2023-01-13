89bb206b894c7af97bf256cb9d7230c322641261
McAteer, Jeffrey P CIV STRATCOM JWAC (US) <jeffrey.p.mcateer.civ@mail.mil>
2023-01-13 13:14:39 -0500

    'added

--- demo_tool/__init__.py
+++ demo_tool/__init__.py
@@ -7,6 +7,18 @@ import subprocess
 import time
 import signal
 import threading
+import traceback
+
+# https://github.com/PyAV-Org/PyAV
+try:
+    import av
+except:
+    traceback.print_exc()
+    subprocess.run([
+        sys.executable, *('-m pip install --user av'.split())
+    ], check=False)
+    import av
+
 
 exit_flag = False
 def signal_handler(signal, frame):
@@ -31,22 +43,34 @@ def main(args=sys.argv):
         os.makedirs(SHARED_DEMO_FOLDER, exist_ok=True)
 
     demo_dir = os.path.join(SHARED_DEMO_FOLDER, demo_name)
-    if os.path.exists(demo_dir):
+    os.makedirs(demo_dir, exist_ok=True)
+
+    demo_video_f = os.path.join(demo_dir, demo_name+'.mp4')
+    print(f'Recording desktop to {demo_video_f}')
+
+    if os.path.exists(demo_dir) and os.path.exists(demo_video_f):
         choice = input(f'{demo_dir} already exists, append (a) to existing demo, replace demo (r), or cancel (c) ?')
         choice = choice.lower().strip()
         if 'c' in choice:
             print('Cancelling...')
             return
-    
-    os.makedirs(demo_dir, exist_ok=True)
+        elif 'r' in choice:
+            print('Replacing...')
+            time.sleep(1)
+            if exit_flag:
+                return
+            os.remove(demo_video_f)
+        else:
+            print('Appending...') # Default behavior
+
     
     while not exit_flag:
         time.sleep(0.5)
         print('Doing work...')
 
 
-    print(f'Opening {SHARED_DEMO_FOLDER}')
-    os.startfile(os.path.realpath(SHARED_DEMO_FOLDER))
+    print(f'Opening {demo_dir}')
+    os.startfile(os.path.realpath(demo_dir))
 
 
 
